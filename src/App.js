046c21591d11c6bc24cb9032920108d86f07375a
jTCode2408 <jasmine.terry91@gmail.com>
2020-03-07 17:07:18 -0500

    downloading dependencies, pkge json, adding scripts. eslint, pprettier, gitignore file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement('div', {}, [
-            React.createElement('h1', {}, name),
-            React.createElement('h2', {}, animal),
-            React.createElement('h2',{}, breed ),
-        ])
-    
-}
-const App = ()=> {
-return React.createElement( "div", {}, [
-            React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: 'Drogo', animal: 'dog', breed: 'Aussie' }),
-            
-    React.createElement(Pet, { name: 'Giselle', animal: 'dog', breed: 'question' }),
-            
-    React.createElement(Pet, { name: 'Dutch', animal: 'dog', breed: 'Pit' })    
-        ])
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Drogo", animal: "dog", breed: "Aussie" }),
+    React.createElement(Pet, {
+      name: "Giselle",
+      animal: "dog",
+      breed: "question"
+    }),
+    React.createElement(Pet, { name: "Dutch", animal: "dog", breed: "Pit" })
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
-
