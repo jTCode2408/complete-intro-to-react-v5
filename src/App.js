e9dd6bcaff3bf29ee716e910b3cd5cb6983b1df5
jTCode2408 <jasmine.terry91@gmail.com>
2020-03-07 16:30:31 -0500

    first commit point in video. vanilla react component w/props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
-import ThemeContext from "./ThemeContext";
-
-const App = () => {
-  const theme = useState("darkblue");
-  return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    </ThemeContext.Provider>
-  );
+const Pet = ({name, animal, breed}) => {
+    return React.createElement('div', {}, [
+            React.createElement('h1', {}, name),
+            React.createElement('h2', {}, animal),
+            React.createElement('h2',{}, breed ),
+        ])
+    
+}
+const App = ()=> {
+return React.createElement( "div", {}, [
+            React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: 'Drogo', animal: 'dog', breed: 'Aussie' }),
+            
+    React.createElement(Pet, { name: 'Giselle', animal: 'dog', breed: 'question' }),
+            
+    React.createElement(Pet, { name: 'Dutch', animal: 'dog', breed: 'Pit' })    
+        ])
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
