ef9043be8eea36bb2337ca472768e204faf3521b
jTCode2408 <jasmine.terry91@gmail.com>
2020-03-07 22:02:48 -0500

    converting to jsx. eslint config to work with react

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Drogo", animal: "dog", breed: "Aussie" }),
-    React.createElement(Pet, {
-      name: "Giselle",
-      animal: "dog",
-      breed: "question"
-    }),
-    React.createElement(Pet, { name: "Dutch", animal: "dog", breed: "Pit" })
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, { name: "Drogo", animal: "dog", breed: "Aussie" }),
+  //     React.createElement(Pet, {
+  //       name: "Giselle",
+  //       animal: "dog",
+  //       breed: "question"
+  //     }),
+  //     React.createElement(Pet, { name: "Dutch", animal: "dog", breed: "Pit" })
+  //   ]);
+  return (
+    <div>
+      <h1> Adopt Me!</h1>
+      <Pet name="Drogo" animal="Dog" breed= "Aussie" />
+      <Pet name="Giselle" animal="Dog" breed= "Unknown" />
+      <Pet name="Dutch" animal="Dog" breed= "Pit" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/> , document.getElementById("root"));
